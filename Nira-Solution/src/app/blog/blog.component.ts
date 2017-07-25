286ab038e39104f9baf5dd9c66d35094bb64baad
nicholas.grill@hobex.at <nicholas.grill@hobex.at>
2017-07-25 14:20:01 +0200

    new entries

--- Nira-Solution/src/app/blog/blog.component.ts
+++ Nira-Solution/src/app/blog/blog.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
-
+  blogEntries:any =
+    [
+      {
+        from:'Angular',
+        subject:'Performance testing',
+        content:'Die gute alte performance...'
+      }
+    ];
   constructor() { }
 
   ngOnInit() {
--- Nira-Solution/src/app/contact/contact.component.ts
+++ Nira-Solution/src/app/contact/contact.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(EMAIL_REGEX)]);
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
