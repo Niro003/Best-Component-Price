94bc8e16cfef2ba1608c24255159253ec97ba280
Nicholas Grill <niko.grill@gmail.com>
2018-10-16 16:25:29 +0200

    remove deprecated bat

--- startNodeNiraSolution.bat
+++
@@ -1,9 +0,0 @@
-if not "%minimized%"=="" goto :minimized
-set minimized=true
-@echo off
-
-cd "C:\Users\Nico\Documents\Projects\Nira-Solution\api"
-
-start /min cmd /C "npm start"
-goto :EOF
-:minimized
